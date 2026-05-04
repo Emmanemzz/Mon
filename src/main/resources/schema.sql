c3594917c9f57aab391f528095447ec616d1bb96
Emmanemzz <emmarondinelli.ts@gmail.com>
2026-05-04 20:57:21 +0200

    Fix schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `mon_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `mon_db`;
+-- CREATE DATABASE  IF NOT EXISTS `mon_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- USE `mon_db`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mon_db
