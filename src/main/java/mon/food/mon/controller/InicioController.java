25c1d31703de44b3b8d9480c133a19d9198b2382
Emmanemzz <emmarondinelli.ts@gmail.com>
2026-05-05 16:42:36 +0200

    Modificar index para ver solo 2 recetas guardadas

--- src/main/java/mon/food/mon/controller/InicioController.java
+++ src/main/java/mon/food/mon/controller/InicioController.java
@@ -24,30 +24,38 @@ public class InicioController {
     private UsuarioService usuarioService;
 
     @GetMapping("/")
-    public String inicio(@AuthenticationPrincipal Usuario usuarioActual, Model model) {
+    public String inicio(@AuthenticationPrincipal Usuario usuarioPrincipal, Model model) {
         List<Receta> destacadas = recetaService.listarDestacadas();
         model.addAttribute("destacadas", destacadas);
 
-        if (usuarioActual != null) {
-            Usuario usuarioFresh = usuarioService.buscarPorEmail(usuarioActual.getUsername());
-            model.addAttribute("recetasGuardadas", usuarioFresh.getRecetasGuardadas());
+        if (usuarioPrincipal != null) {
+            Usuario usuarioActual = usuarioService.buscarPorEmail(usuarioPrincipal.getUsername());
+            List<Receta> todasGuardadas = new ArrayList<>(usuarioActual.getRecetasGuardadas());
+            List<Receta> guardadasLimitadas = todasGuardadas.size() > 2 ? todasGuardadas.subList(0, 2) : todasGuardadas;
+            model.addAttribute("recetasGuardadas", guardadasLimitadas);
+            model.addAttribute("totalGuardadas", todasGuardadas.size());
         } else {
             model.addAttribute("recetasGuardadas", new ArrayList<>());
+            model.addAttribute("totalGuardadas", 0);
         }
 
         return "index";
     }
 
     @GetMapping("/inicio")
-    public String inicioPorNombre(@AuthenticationPrincipal Usuario usuarioActual, Model model) {
+    public String inicioPorNombre(@AuthenticationPrincipal Usuario usuarioPrincipal, Model model) {
         List<Receta> destacadas = recetaService.listarDestacadas();
         model.addAttribute("destacadas", destacadas);
 
-        if (usuarioActual != null) {
-            Usuario usuarioFresh = usuarioService.buscarPorEmail(usuarioActual.getUsername());
-            model.addAttribute("recetasGuardadas", usuarioFresh.getRecetasGuardadas());
+        if (usuarioPrincipal != null) {
+            Usuario usuarioActual = usuarioService.buscarPorEmail(usuarioPrincipal.getUsername());
+            List<Receta> todasGuardadas = new ArrayList<>(usuarioActual.getRecetasGuardadas());
+            List<Receta> guardadasLimitadas = todasGuardadas.size() > 2 ? todasGuardadas.subList(0, 2) : todasGuardadas;
+            model.addAttribute("recetasGuardadas", guardadasLimitadas);
+            model.addAttribute("totalGuardadas", todasGuardadas.size());
         } else {
             model.addAttribute("recetasGuardadas", new ArrayList<>());
+            model.addAttribute("totalGuardadas", 0);
         }
 
         return "index";
